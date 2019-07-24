907dcca7b444dabd571e10ef26bb6078337ae2d5
Paul Colombier <paul.colombier@epsi.fr>
2019-07-24 12:14:06 +0200

    -UX -Fonctionnality List for Add and Delete -Displaying a list on several lines -Bugfix -The hedgehog is still there :)

--- hello world/ViewController.swift
+++ hello world/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  hello world
+//  List
 //
 //  Created by Paul Colombier on 19/02/2019.
 //  Copyright © 2019 Paul Colombier. All rights reserved.
@@ -12,13 +12,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var userTextField: UITextField!
     @IBOutlet weak var repeatLabel: UILabel!
     
-    @IBAction func repeatButtonTouched() {
-        repeatLabel.text = userTextField.text
-        userTextField.text = ""
+    var repeatField = ""
+    var userField = ""
+
+    @IBAction func addButtonTouched() {
+        repeatField = String(repeatLabel.text!)
+        userField = String(userTextField.text!)
+        repeatField = (userField + "\n" + repeatField)
+        print(repeatField)
+        userField = ""
+        repeatLabel.text = repeatField
+        userTextField.text = userField
+    }
+    
+    @IBAction func clearButtonTouched() {
+        repeatLabel.text = ""
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Close iOS Keyboard by touching anywhere
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapGesture)
+        //If tap gesture blocked some other touches, then add this line :
+        //tapGesture.cancelsTouchesInView = false
     }
 }
 
